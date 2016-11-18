fecc7d525ecc40262945d05b4b48d3b2e24211b9
ravelasquez99 <ravelasquez99@gmail.com>
2016-11-17 18:57:25 -0800

    -setup model view controller. Also setup initial mapVC class

---
+++ SpaceIn/SpaceIn/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  SpaceIn
+//
+//  Created by Richard Velazquez on 11/17/16.
+//  Copyright © 2016 Ricky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        fatalError("You are using too much memory")
+    }
+}
