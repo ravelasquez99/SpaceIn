30fc62287d3f52facec9ea6316f80704620d6dae
ravelasquez99 <ravelasquez99@gmail.com>
2016-11-19 16:10:52 -0800

    -map is centered on a location

---
+++ SpaceIn/SpaceIn/MapOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  MapOverlayView.swift
+//  SpaceIn
+//
+//  Created by Richard Velazquez on 11/19/16.
+//  Copyright © 2016 Ricky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+protocol MapOverlayViewDelegate {
+    
+}
+class MapOverlayView: UIView {
+    var delegate: MapOverlayViewDelegate?
+}
--- SpaceIn/SpaceIn/MapViewController.swift
+++ SpaceIn/SpaceIn/MapViewController.swift
@@ -15,16 +15,47 @@ import Mapbox
 class MapViewController: UIViewController, MGLMapViewDelegate {
     
     @IBOutlet var mapView: MGLMapView!
+    @IBOutlet private var mapOverlayView: MapOverlayView!
+    
+    private var didConstrainMapOverlayView = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupMapOverlayView()
+        self.setupMapView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.centerMap()
+    }
+    
+    private func setupMapOverlayView () {
+        self.mapOverlayView.delegate = self
+        self.mapOverlayView.isUserInteractionEnabled = false
+    }
+    private func setupMapView () {
         self.mapView.delegate = self
         self.mapView.showsUserLocation = true
+    }
+    
+    private func centerMap() {
+        //37.827791, -122.255903
+        let coordinate = CLLocationCoordinate2DMake(37.827791, -122.255903)
+        self.mapView.setCenter(coordinate, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         fatalError("You are using too much memory")
     }
+    
+}
+
+extension MapViewController: MapOverlayViewDelegate {
     
 }
