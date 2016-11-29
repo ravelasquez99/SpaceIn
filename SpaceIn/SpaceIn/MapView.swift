57d011ab01acf2f8a14f029fcf872d9e13772394
ravelasquez99 <ravelasquez99@gmail.com>
2016-11-28 20:47:44 -0800

    -did a bit of refactoring

---
+++ SpaceIn/SpaceIn/MapView.swift
@@ -0,0 +1,99 @@
+//
+//  MapView.swift
+//  SpaceIn
+//
+//  Created by Richard Velazquez on 11/26/16.
+//  Copyright © 2016 Ricky. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
+// moving anos http://stackoverflow.com/questions/8564013/animate-removal-of-annotations/8564097#8564097
+
+//http://stackoverflow.com/questions/29776549/animate-mapkit-annotation-coordinate-change-in-swift
+class MapView: MKMapView {
+    
+    static let distance: CLLocationDistance = 650
+    static let pitch: CGFloat = 65
+    static let heading = 0.0
+    
+    let coordinate = CLLocationCoordinate2D(latitude: 37.827774,longitude:  -122.255828)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+        self.camera = self.createDefaultCamera()
+        self.placeDefaultPins()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+        self.camera = self.createDefaultCamera()
+        self.placeDefaultPins()
+    }
+    
+    private func setup() {
+        self.mapType = .hybridFlyover
+        self.showsBuildings = true
+        self.showsPointsOfInterest = false
+        self.showsTraffic = true
+        self.delegate = self
+    }
+    
+    private func placeDefaultPins() {
+
+        let ano = MKPointAnnotation()
+        ano.coordinate = coordinate
+        self.addAnnotation(ano)
+    }
+    
+    private func createDefaultCamera()-> MKMapCamera {
+        return MKMapCamera(lookingAtCenter: coordinate,
+                              fromDistance: MapView.distance,
+                              pitch: MapView.pitch,
+                              heading: MapView.heading)
+    }
+    
+    private func setUserInteraction() {
+        self.isZoomEnabled = true
+        self.isRotateEnabled = false
+        self.isScrollEnabled = false
+    }
+}
+
+
+extension MapView: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+        
+        // Better to make this class property
+        let annotationIdentifier = "AnnotationIdentifier"
+        
+        var annotationView: MKAnnotationView?
+        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
+            annotationView = dequeuedAnnotationView
+            annotationView?.annotation = annotation
+        }
+        else {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        if let annotationView = annotationView {
+            // Configure your annotation view here
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "Kobe")
+        }
+        
+        annotationView?.frame = CGRect(x: 0, y: 0, width: 75, height: 75)
+        annotationView?.contentMode = .scaleAspectFit
+        
+        return annotationView
+    }
+    
+}
--- SpaceIn/SpaceIn/MapViewController.swift
+++ SpaceIn/SpaceIn/MapViewController.swift
@@ -14,76 +14,16 @@ import MapKit
 
 class MapViewController: UIViewController {
     
-    @IBOutlet var mapView: MKMapView!
-    
-    let distance: CLLocationDistance = 650
-    let pitch: CGFloat = 65
-    let heading = 0.0
-    var camera: MKMapCamera?
+    @IBOutlet var mapView: MapView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.mapType = .satelliteFlyover
-        mapView.showsBuildings = true
-        mapView.showsPointsOfInterest = false
-        mapView.showsTraffic = false
-        mapView.delegate = self
-        
-        let coordinateTwo = CLLocationCoordinate2D(latitude: 40.7784405,
-                                                   longitude: -73.9856644)
-        
-        //let coordinate = CLLocationCoordinate2D(latitude: 40.7484405,
-                                             //   longitude: -73.9856644)
-        camera = MKMapCamera(lookingAtCenter: coordinateTwo,
-                             fromDistance: distance,
-                             pitch: pitch,
-                             heading: heading)
-        mapView.camera = camera!
-        
 
-        let ano = MKPointAnnotation()
-        ano.coordinate = coordinateTwo
-        self.mapView.addAnnotation(ano)
-        
-        self.mapView.isZoomEnabled = true
-        self.mapView.isRotateEnabled = false
-        self.mapView.isScrollEnabled = false
     }
     
     @IBAction func animate(_ sender: UIButton) {
     }
 }
 
-extension MapViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard !(annotation is MKUserLocation) else {
-            return nil
-        }
-        
-        // Better to make this class property
-        let annotationIdentifier = "AnnotationIdentifier"
-        
-        var annotationView: MKAnnotationView?
-        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
-            annotationView = dequeuedAnnotationView
-            annotationView?.annotation = annotation
-        }
-        else {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        
-        if let annotationView = annotationView {
-            // Configure your annotation view here
-            annotationView.canShowCallout = true
-            annotationView.image = UIImage(named: "Kobe")
-        }
-        
-        annotationView?.frame = CGRect(x: 0, y: 0, width: 75, height: 75)
-        annotationView?.contentMode = .scaleAspectFit
-        
-        return annotationView
-    }
-    
-}
 
