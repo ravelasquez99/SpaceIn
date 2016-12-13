b3afdb6c2bd605847837e2ecefe9ee8d40fdaea0
ravelasquez99 <ravelasquez99@gmail.com>
2016-12-12 18:19:00 -0800

    -topless text field now has bottom

--- SpaceIn/SpaceIn/ToplessTextfield.swift
+++ SpaceIn/SpaceIn/ToplessTextfield.swift
@@ -36,7 +36,7 @@ class ToplessTextField: UITextField {
             let border = CALayer()
             let width = CGFloat(2.0)
             border.borderColor = UIColor.blue.cgColor
-            border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+            border.frame = CGRect(x: 0, y: self.frame.size.height - 20.0, width:  self.frame.size.width, height: 2)
             border.borderWidth = width
             self.layer.addSublayer(border)
             self.layer.masksToBounds = true
