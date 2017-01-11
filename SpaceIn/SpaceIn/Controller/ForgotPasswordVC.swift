444d59655ed9c4fd4be1548c33170c57aedf5f8c
ravelasquez99 <ravelasquez99@gmail.com>
2017-01-10 20:17:56 -0800

    -first commit for this task

---
+++ SpaceIn/SpaceIn/Controller/ForgotPasswordVC.swift
@@ -0,0 +1,36 @@
+//
+//  ForgotPasswordVC.swift
+//  SpaceIn
+//
+//  Created by Richard Velazquez on 1/10/17.
+//  Copyright © 2017 Ricky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ForgotPasswordVC: UIViewController {
+    //Constants
+    let backButton = UIButton()
+    let loginImageView = UIImageView()
+    let troubleLogginInLabel = UILabel()
+    let 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
+
+
+//UI Related
+extension ForgotPasswordVC {
+    func addSubviews() {
+        
+    }
+}
