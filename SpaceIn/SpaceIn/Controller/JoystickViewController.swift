5568ecbf88ff38181316c743adc444d859b16a2a
ravelasquez99 <ravelasquez99@gmail.com>
2017-02-24 06:58:03 -0800

    joystick fades

--- SpaceIn/SpaceIn/Controller/JoystickViewController.swift
+++ SpaceIn/SpaceIn/Controller/JoystickViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class JoystickViewController: UIViewController {
     let joyStickView = JoyStickView(frame: CGRect.zero)
 
-    var didSetup = false
     fileprivate let threeDButton = RoundedButton(filledIn: false, color: UIColor.white)
     fileprivate let minusButton = RoundedButton(filledIn: false, color: UIColor.white)
     fileprivate let plusButton = RoundedButton(filledIn: false, color: UIColor.white)
     fileprivate let notificationsButton = RoundedButton(filledIn: false, color: UIColor.white)
+    fileprivate let profileContainerButton = RoundedButton(filledIn: false, color: UIColor.white)
     fileprivate let profileButton = RoundedButton(filledIn: false, color: UIColor.clear) //for profile pictures the padding between the border and the image isn't happening so we have to wrap it in a circular view
+    
+    fileprivate var didSetup = false
+    fileprivate var isShowingButtons = false
     
     static let paddingFromJoystick: CGFloat = 12
     static let joystickMiniButtonMultiplier: CGFloat = 0.55
@@ -32,6 +35,36 @@ class JoystickViewController: UIViewController {
     }
 }
 
+///MARK:- User Interaction
+extension JoystickViewController: JoystickViewDelegate {
+    func tappedJoyStick() {
+        self.showButtons(show: !self.isShowingButtons)
+        print("we tapped the joystick")
+    }
+    
+    fileprivate func showButtons(show: Bool) {
+        if self.didSetup { //so we don't call this in the initialization
+            self.joyStickView.isUserInteractionEnabled = false
+            
+            if self.isShowingButtons {
+                UIView.animate(withDuration: 2.0, animations: { 
+                    self.hideAndDisableButtons()
+                }, completion: { (done) in
+                    self.joyStickView.isUserInteractionEnabled = true
+                    self.isShowingButtons = false
+                })
+            } else {
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.showAndEnableButtons()
+                }, completion: { (done) in
+                    self.joyStickView.isUserInteractionEnabled = true
+                    self.isShowingButtons = true
+                })
+            }
+        }
+    }
+}
+
 
 //MARK: - UI Setup
 extension JoystickViewController {
@@ -40,6 +73,7 @@ extension JoystickViewController {
             self.didSetup = true
             self.setupJoystick()
             self.setupButtons()
+            self.didSetup = true
         }
     }
     
@@ -48,6 +82,7 @@ extension JoystickViewController {
         joyStickView.constrainWidthAndHeightToValueAndActivate(value: 80)
         joyStickView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         joyStickView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -15).isActive = true
+        self.joyStickView.delegate = self
     }
     
     private func setupButtons() {
@@ -60,56 +95,100 @@ extension JoystickViewController {
     }
     
     private func layoutButtons() {
+        self.constrainThreeDButton()
+        self.setupProfileButton()
+        self.constrainNotificationsButton()
+        self.constrainPlusAndMinusButtons()
+        self.hideAndDisableButtons()
+    }
+    
+    fileprivate func hideAndDisableButtons() {
+        self.profileContainerButton.alpha = 0
+        self.profileButton.alpha = 0
+        self.minusButton.alpha = 0
+        self.plusButton.alpha = 0
+        self.notificationsButton.alpha = 0
+        self.threeDButton.alpha = 0
+        
+        self.profileContainerButton.isUserInteractionEnabled = false
+        self.profileButton.isUserInteractionEnabled = false
+        self.minusButton.isUserInteractionEnabled = false
+        self.plusButton.isUserInteractionEnabled = false
+        self.notificationsButton.isUserInteractionEnabled = false
+        self.threeDButton.isUserInteractionEnabled = false
+    }
+    
+    fileprivate func showAndEnableButtons() {
+        self.profileContainerButton.alpha = 1
+        self.profileButton.alpha = 1
+        self.minusButton.alpha = 1
+        self.plusButton.alpha = 1
+        self.notificationsButton.alpha = 1
+        self.threeDButton.alpha = 1
+        
+        self.profileContainerButton.isUserInteractionEnabled = true
+        self.profileButton.isUserInteractionEnabled = true
+        self.minusButton.isUserInteractionEnabled = true
+        self.plusButton.isUserInteractionEnabled = true
+        self.notificationsButton.isUserInteractionEnabled = true
+        self.threeDButton.isUserInteractionEnabled = true
+    }
+    
+    private func constrainNotificationsButton() {
+        let notificationImage = UIImage(named: AssetName.notification.rawValue)
+        self.setupRounded(button: self.notificationsButton, withImage: notificationImage)
+        self.notificationsButton.centerYAnchor.constraint(equalTo: self.threeDButton.centerYAnchor).isActive = true
+        self.notificationsButton.leftAnchor.constraint(equalTo: self.joyStickView.rightAnchor, constant: JoystickViewController.paddingFromJoystick * 2).isActive = true
+    }
+    
+    private func constrainPlusAndMinusButtons() {
+        let plusImage = UIImage(named: AssetName.zoomIn.rawValue)
+        let minusImage = UIImage(named: AssetName.zoomOut.rawValue)
+
+        self.setupRounded(button: self.plusButton, withImage: plusImage)
+        self.setupRounded(button: self.minusButton, withImage: minusImage)
+
+        self.plusButton.centerYAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick / 2).isActive = true
+        self.minusButton.centerYAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick / 2).isActive = true
+        
+        self.plusButton.rightAnchor.constraint(equalTo: self.notificationsButton.centerXAnchor, constant: -5).isActive = true
+        self.minusButton.leftAnchor.constraint(equalTo: self.threeDButton.centerXAnchor, constant: 5).isActive = true
+    }
+    
+    private func constrainThreeDButton() {
         //setup one button
         let threeDImage = UIImage(named: AssetName.threeDCircle.rawValue)
         self.setupRounded(button: self.threeDButton, withImage: threeDImage)
         self.threeDButton.rightAnchor.constraint(equalTo: self.joyStickView.leftAnchor, constant: -JoystickViewController.paddingFromJoystick * 2).isActive = true
         self.threeDButton.centerYAnchor.constraint(equalTo: self.joyStickView.centerYAnchor, constant: 5).isActive = true
         
-        let containerButton = RoundedButton(filledIn: false, color: UIColor.white)
-        containerButton.isUserInteractionEnabled = false
-        containerButton.translatesAutoresizingMaskIntoConstraints = false
-        containerButton.titleLabel?.text = ""
-        containerButton.backgroundColor = UIColor.clear
-        self.view.addSubview(containerButton)
-        self.setupRounded(button: containerButton, withImage: nil)
-        containerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        containerButton.bottomAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick - 5).isActive = true
+    }
+    
+    private func setupProfileButton() {
+        self.profileContainerButton.isUserInteractionEnabled = false
+        self.profileContainerButton.translatesAutoresizingMaskIntoConstraints = false
+        self.profileContainerButton.titleLabel?.text = ""
+        self.profileContainerButton.backgroundColor = UIColor.clear
+        self.view.addSubview(self.profileContainerButton)
+        self.setupRounded(button: profileContainerButton, withImage: nil)
+        self.profileContainerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.profileContainerButton.bottomAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick - 5).isActive = true
         
         
         let profileImage = UIImage(named: AssetName.rickyHeadshot.rawValue)
         self.profileButton.setImage(profileImage, for: .normal)
         self.profileButton.imageView?.contentMode = .scaleAspectFit
         
-        containerButton.addSubview(self.profileButton)
+        profileContainerButton.addSubview(self.profileButton)
         
         self.profileButton.translatesAutoresizingMaskIntoConstraints = false
-        self.profileButton.widthAnchor.constraint(equalTo: containerButton.widthAnchor, constant: -5).isActive = true
-        self.profileButton.heightAnchor.constraint(equalTo: containerButton.heightAnchor, constant: -5).isActive = true
-        self.profileButton.centerXAnchor.constraint(equalTo: containerButton.centerXAnchor).isActive = true
-        self.profileButton.centerYAnchor.constraint(equalTo: containerButton.centerYAnchor).isActive = true
-
+        self.profileButton.widthAnchor.constraint(equalTo: profileContainerButton.widthAnchor, constant: -5).isActive = true
+        self.profileButton.heightAnchor.constraint(equalTo: profileContainerButton.heightAnchor, constant: -5).isActive = true
+        self.profileButton.centerXAnchor.constraint(equalTo: profileContainerButton.centerXAnchor).isActive = true
+        self.profileButton.centerYAnchor.constraint(equalTo: profileContainerButton.centerYAnchor).isActive = true
+        
         self.profileButton.layer.borderWidth = 0.0
-        
-        
-    
-        let notificationImage = UIImage(named: AssetName.notification.rawValue)
-        self.setupRounded(button: self.notificationsButton, withImage: notificationImage)
-        self.notificationsButton.centerYAnchor.constraint(equalTo: self.threeDButton.centerYAnchor).isActive = true
-        self.notificationsButton.leftAnchor.constraint(equalTo: self.joyStickView.rightAnchor, constant: JoystickViewController.paddingFromJoystick * 2).isActive = true
-        
-        let plusImage = UIImage(named: AssetName.zoomIn.rawValue)
-        self.setupRounded(button: self.plusButton, withImage: plusImage)
-        self.plusButton.centerYAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick / 2).isActive = true
-        self.plusButton.rightAnchor.constraint(equalTo: self.notificationsButton.centerXAnchor, constant: -5).isActive = true
-        
-        let minusImage = UIImage(named: AssetName.zoomOut.rawValue)
-        self.setupRounded(button: self.minusButton, withImage: minusImage)
-        self.minusButton.centerYAnchor.constraint(equalTo: self.joyStickView.topAnchor, constant: -JoystickViewController.paddingFromJoystick / 2).isActive = true
-        self.minusButton.leftAnchor.constraint(equalTo: self.threeDButton.centerXAnchor, constant: 5).isActive = true
-        
-        //alpha
-        
+
     }
     
     private func setupRounded(button: RoundedButton, withImage image: UIImage?) {
@@ -124,7 +203,7 @@ extension JoystickViewController {
     
     //convenience function
     private func buttons() -> [UIButton] {
-        return [self.threeDButton, self.minusButton, self.plusButton, self.notificationsButton]
+        return [self.threeDButton, self.minusButton, self.plusButton, self.notificationsButton] //does not include profile button because it is setup a little differently
     }
     
 
